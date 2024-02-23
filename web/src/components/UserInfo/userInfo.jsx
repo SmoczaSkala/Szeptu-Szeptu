daa6940e8ee847c31c44e6d22d65e832d940089d
SmoczaSkala <99537042+SmoczaSkala@users.noreply.github.com>
2024-02-23 13:51:12 +0100

    app header, user info body

--- web/src/components/UserInfo/userInfo.jsx
+++ web/src/components/UserInfo/userInfo.jsx
@@ -2,8 +2,27 @@ import React from "react";
 
 const userInfo = () => {
   return (
-    <div>
-      <p>UserInfo</p>
+    <div className="userInfo">
+      <div className="user">
+        <img src="" />
+        <h2>User Name</h2>
+        <div className="status">
+          <p></p>
+          <img className="status-icon" src="" />
+        </div>
+      </div>
+      <div className="favorite">
+        <img src="" />
+        <p>Add to favorite</p>
+      </div>
+      <div className="block">
+        <img src="" />
+        <p>Block user</p>
+      </div>
+      <div className="view-profile">
+        <img src="" />
+        <p>View Profile</p>
+      </div>
     </div>
   );
 };
