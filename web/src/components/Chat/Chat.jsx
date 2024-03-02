69ba78a864223a0698b16b05b7fa27e471f85ceb
SmoczaSkala <kamilo.dance2000@gmail.com>
2024-03-02 09:44:27 +0100

    icons

--- web/src/components/Chat/Chat.jsx
+++ web/src/components/Chat/Chat.jsx
@@ -5,16 +5,18 @@ const Chat = () => {
   return (
     <div className="chat">
       <div className="chat-user">
-        <img className="profile" src="" alt="" />
-        <h1>Jan Kowalski</h1>
-        <h6 className="status">Online</h6>
-        <a href="">
-          <img className="call" src="" alt="" />
-        </a>
+        <div>
+          <img className="profile" src="" alt="" />
+          <h1>Jan Kowalski</h1>
+          <h6 className="status">Online</h6>
+        </div>
+        <div className="right-side-bar">
+          <img className="call-icon" src="" alt="" />
+        </div>
       </div>
       <div className="send-message">
         <a href="">
-          <img className="add-photo" src="" alt="" />
+          <img className="add-photo" src="/upload.png" alt="" />
         </a>
         <input type="text" placeholder="Type a message here" />
         <a href="">
