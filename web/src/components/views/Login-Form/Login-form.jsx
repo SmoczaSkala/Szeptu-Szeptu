20af0d0062dfb1fc1c5550245c3c2a18fc2a082a
SmoczaSkala <99537042+SmoczaSkala@users.noreply.github.com>
2024-03-02 09:20:38 +0100

    commit

--- web/src/components/views/Login-Form/Login-form.jsx
+++ web/src/components/views/Login-Form/Login-form.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Login from "../../Login/Login";
 import RegisterPhoto from "../../FormPhoto/RegisterPhoto";
-import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
   return (
