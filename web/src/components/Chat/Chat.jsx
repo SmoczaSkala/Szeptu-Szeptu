fea797068dff8056808be2735985b71005f83b2d
Mateusz <u74_matmar_waw@technischools.com>
2024-04-03 09:41:46 +0200

    wyświetlanie wiadomości (fronted part. 2)

--- web/src/components/Chat/Chat.jsx
+++ web/src/components/Chat/Chat.jsx
@@ -1,9 +1,50 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useParams } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 import "./../../scss/Chat.scss";
+import ReceivedMessages from "../Messages/ReceivedMessages";
+import SentMessages from "../Messages/SentMessages";
 
 const Chat = () => {
   const [message, setMessage] = useState("");
+  const [receivedMessages, setReceivedMessages] = useState([]);
+  const [sentMessages, setSentMessages] = useState([]);
+  const { userId } = useParams();
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    const fetchMessages = async () => {
+      try {
+        const receivedResponse = await axios.get(
+          `/api/messages/received?token=${sessionStorage.getItem("token")}`
+        );
+        setReceivedMessages(receivedResponse.data.messages);
+
+        const sentResponse = await axios.get(
+          `/api/messages/sent?token${sessionStorage.getItem("token")}`
+        );
+        setSentMessages(sentResponse.data.messages);
+      } catch (error) {
+        console.error(
+          "Błąd podczas pobierania wiadomości:",
+          error.response.data
+        );
+      }
+    };
+
+    fetchMessages();
+  }, [userId]);
+
+  useEffect(() => {
+    const token = sessionStorage.getItem("token");
+
+    if (token) {
+      const decodedToken = jwtDecode(token);
+
+      setUserInfo(decodedToken);
+    }
+  }, []);
 
   const handleMessageChange = (e) => {
     setMessage(e.target.value);
@@ -30,15 +71,19 @@ const Chat = () => {
       <div className="chat-user">
         <div className="left-side">
           <img className="avatar" src="/account.png" alt="avatar" />
-          <h1>Jan Kowalski</h1>
+          {userInfo && <p>{userInfo.name}</p>}
         </div>
         <div className="right-side">
           <img src="/facetime-button.png" alt="" />
           <img src="telephone.png" alt="" />
         </div>
       </div>
-      <div className="messages">{/* tu beda wiadomosci mapowane */}</div>
-      <div className="messages">{/* tu beda wiadomosci mapowane */}</div>
+      <div className="received-messages">
+        <ReceivedMessages messages={receivedMessages} />
+      </div>
+      <div className="sent-messages">
+        <SentMessages messages={sentMessages} />
+      </div>
       <div className="send-message">
         <img
           className="add-photo"
